d8d461a64f18f2e59c38a928569c20212a6e7a16
RSoftLeaf <roma7848774@gmail.com>
2022-08-10 17:18:47 +0300

    Створений клас Grid

---
+++ MySnake/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnake
+{
+    internal class Grid : Button
+    {
+        public bool isSnake { get; set; }
+        public bool isFood { get; set; }
+        public Grid()
+        {
+
+        }
+        public Grid(int SizeX, int SizeY, int StartX, int StartY)
+        {
+            this.Size = new System.Drawing.Size(SizeX, SizeY);
+            this.Location = new System.Drawing.Point(StartX, StartY);
+            this.isSnake = false;
+            this.isFood = false;
+            this.FlatStyle = FlatStyle.Flat;
+            this.BackColor = Color.Silver;
+            
+        }
+
+
+    }
+}
