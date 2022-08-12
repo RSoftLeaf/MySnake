f1a88b8b65b206aea97758d2f7a1ed42e93584d1
RSoftLeaf <roma7848774@gmail.com>
2022-08-12 17:39:00 +0300

    SnakeMove Done()

--- MySnake/MainForm.cs
+++ MySnake/MainForm.cs
@@ -17,14 +17,95 @@ namespace MySnake
         int LocationY = 50;
         //Масиви ігрового поля
         Grid[,] Field;
+        string Direction = "UP";
+        List<SnakeCell> MySnake;
+        //Round
+        bool IsGame = true;
+        bool GameOver = false;
+        //await\async
+        int TaskDelay = 1000;
+        int iTemp = 2;
 
         #endregion
         public MainForm()
         {
             InitializeComponent();
             Initialize();
+            timer1.Interval = TaskDelay;
         }
+        public void StartNewGame()
+        {
+            NewSnakeSet();
+            timer1.Start();
+        }
+        public void StopGame()
+        {
+            
+        }
+        public void SetFood()
+        {
+            Random randim = new Random();
+            while (true)
+            {
+                int randimX = randim.Next(DimX);
+                int randimY = randim.Next(DimY);
+                if (!Field[randimX, randimY].isSnake) 
+                { 
+                    Field[randimX, randimY].SetFood(); 
+                    break; 
+                }
+            } 
 
+        }
+        public void SnakeMove()
+        {
+            int x = MySnake[0].X;
+            int y = MySnake[0].Y;
+            switch (Direction)
+            {
+                case "UP":
+                    {
+                        if (Field[x,y - 1].isFood)
+                            AddSnakeCell(x, y - 1, false);
+                        else
+                            AddSnakeCell(x, y - 1, true);
+                        break;
+                    }
+                case "DOWN":
+                    {
+                        //
+                        break;
+                    }
+            }
+            
+            iTemp += 1;
+        }
+        public void NewSnakeSet()
+        {
+            MySnake = new List<SnakeCell>();
+            int StartX = DimX / 2;
+            int StartY = DimY - 1;
+            //await Task.Delay(TaskDelay);
+            AddSnakeCell(StartX, StartY, false);
+            //await Task.Delay(TaskDelay);
+            AddSnakeCell(StartX, StartY - 1, false);
+            //await Task.Delay(TaskDelay);
+            AddSnakeCell(StartX, StartY - 2, false);
+            //SetFood();
+            Field[StartX, 3].SetFood();
+        }
+        public void AddSnakeCell(int x, int y, bool remove)
+        {
+            MySnake.Insert(0, new SnakeCell(x, y));
+            Field[x, y].SetSnake();
+            if (remove)
+            {
+                int lastX = MySnake[MySnake.Count - 1].X;
+                int lastY = MySnake[MySnake.Count - 1].Y;
+                MySnake.RemoveAt(MySnake.Count - 1);
+                Field[lastX, lastY].Reset();
+            }
+        }
         public void Initialize()
         {
             #region PANEL INITIALIZE
@@ -51,6 +132,20 @@ namespace MySnake
             
             //==========================================================*/
             #endregion
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                Direction = "DOWN";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SnakeMove();
+
         }
     }
 }
